4d34a3061ca72b26d267a5ca3f3dddc251f04ee4
aladiha <34811841+aladiha@users.noreply.github.com>
2020-05-14 06:23:02 +0300

    navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import classes from './NavigationItems.module.css'
+import Item from './NavigationItem/NavigationItem'
 
-const items = (props) => {
-    
-}
+const items = (props) => (
+    <ul className={classes.NavigationItems}>
+        <Item link="/" active>Burger Builder</Item>
+        <Item link="/">Checkout</Item>
+        
+    </ul>
+);
 
 export default items;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import classes from './Toolbar.module.css'
-import Logo from '../../Logo/Logo'
+import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
     
@@ -8,8 +9,7 @@ const toolbar = (props) => {
         <div>Menu</div>
         <Logo/>
         <nav>
-            
-            ...
+            <NavigationItems/>
         </nav>
     </header>
 }
